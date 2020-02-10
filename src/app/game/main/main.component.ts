a22a289074521ba66fbb5520196cc90aa49553e3
Karolis Jocevičius <karolis.jocevicius@gmail.com>
2020-02-10 21:27:22 +0200

    planets viewer

--- src/app/game/main/main.component.ts
+++ src/app/game/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { OrbitBody } from 'src/app/shared/planet-viewer/planet-viewer.component';
 
 @Component({
   selector: 'app-main',
@@ -7,6 +8,36 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None,
 })
 export class MainComponent implements OnInit {
+
+  planets: OrbitBody[] = [
+    {
+      name: 'sun',
+      rad: 30,
+      distance: 0,
+      rv: 0,
+      appearance: {
+        color: 'yellow',
+      },
+    },
+    {
+      name: 'bar',
+      rad: 15,
+      distance: 100,
+      rv: 2,
+      appearance: {
+        color: 'red',
+      },
+    },
+    {
+      name: 'foo',
+      rad: 10,
+      distance: 70,
+      rv: 1,
+      appearance: {
+        color: 'blue',
+      },
+    },
+  ];
 
   constructor() { }
 
--- src/app/shared/planet-viewer/planet-viewer.component.ts
+++ src/app/shared/planet-viewer/planet-viewer.component.ts
@@ -1,4 +1,26 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Input, ViewChild, AfterViewInit, ElementRef, OnChanges, SimpleChanges } from '@angular/core';
+
+export interface OrbitBody {
+  /**
+   * for reference
+   */
+  name: string;
+  /**
+   * Planet radius
+   */
+  rad: number;
+  /**
+   * Planet distance from center
+   */
+  distance: number;
+  /**
+   * radial velocity (deg/sec)
+   */
+  rv: number;
+  appearance: {
+    color: string;
+  };
+}
 
 @Component({
   selector: 'app-planet-viewer',
@@ -6,11 +28,73 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./planet-viewer.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class PlanetViewerComponent implements OnInit {
+export class PlanetViewerComponent implements OnInit, AfterViewInit, OnChanges {
+
+  @Input() height = 400;
+  @Input() width = 400;
+  @Input() planets: OrbitBody[];
+
+  @ViewChild('container') private container: ElementRef;
+  private canvas: HTMLCanvasElement;
+  private ctx2d: CanvasRenderingContext2D;
+  private lastFrameTime: number;
+
+  //state
+  private planetsAngle: number[] = [];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.height || changes.width) {
+      this.setupCanvasSize();
+    } else if (changes.planets) {
+      this.planetsAngle = this.planets.map(p => 0);
+    }
+  }
+
+  ngAfterViewInit() {
+    this.canvas = document.createElement('canvas');
+    this.ctx2d = this.canvas.getContext('2d');
+    this.container.nativeElement.appendChild(this.canvas);
+    this.setupCanvasSize();
+
+    this.lastFrameTime = Date.now();
+    requestAnimationFrame(() => this.draw());
+  }
+
+  private setupCanvasSize() {
+    this.canvas.width = this.width = this.width || this.container.nativeElement.clientWidth;
+    this.canvas.height = this.height = this.height || this.container.nativeElement.clientHeight;
+    this.ctx2d.translate(this.width / 2, this.height / 2);
+  }
+
+  private draw() {
+    this.ctx2d.fillRect(-this.width / 2, -this.height / 2, this.width, this.height);
+
+    const now = Date.now();
+    const dts = (now - this.lastFrameTime) / 1000;
+    this.planets.forEach((planet, idx) => {
+      let theta = 0;
+      this.planetsAngle[idx] += planet.rv / Math.PI * dts;
+      theta = this.planetsAngle[idx];
+      const x = planet.distance * Math.cos(theta);
+      const y = planet.distance * Math.sin(theta);
+      this.drawPlanet(this.ctx2d, x, y, planet);
+    });
+    this.lastFrameTime = now;
+    requestAnimationFrame(() => this.draw());
+  }
+
+  private drawPlanet(ctx2d: CanvasRenderingContext2D, x: number, y: number, planet: OrbitBody) {
+    ctx2d.save();
+    ctx2d.fillStyle = planet.appearance.color;
+    ctx2d.translate(x, y);
+    ctx2d.beginPath();
+    ctx2d.arc(0, 0, planet.rad, 0, 2 * Math.PI, false);
+    ctx2d.fill();
+    ctx2d.restore();
+  }
 }
